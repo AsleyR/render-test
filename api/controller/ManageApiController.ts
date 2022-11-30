63ecfe72307547bc37f0c9d38a0f211ca0ba41b4
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-11-29 23:08:22 -0600

    Possible fix of broken headers

--- api/controller/ManageApiController.ts
+++ api/controller/ManageApiController.ts
@@ -6,8 +6,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 // Controllers
-exports.manageApi = (req: Request, res: Response) => {
-    res.header("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL_CONNECTION}`)
+exports.manageApi = (req: Request, res: Response, next: any) => {
+    res.header("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL_CONNECTION}`);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.status(200).send({msg: "Test message. Backend Server Working Correctly."})
+    next()
 }
