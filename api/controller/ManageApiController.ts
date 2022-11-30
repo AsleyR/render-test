1394f90789de903e4373c30218f392866d2b59b0
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-11-29 20:43:57 -0600

    Change api message

--- api/controller/ManageApiController.ts
+++ api/controller/ManageApiController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
 
 exports.manageApi = (req: Request, res: Response) => {
-    res.status(200).send({msg: "TEST MESSAGE EXAMPLE BUAH HAHAHAHA"})
+    res.status(200).send({msg: "Test message. Backend Server Working Correctly."})
 }
