c1aae7b84a062f19b6dd93d5ff61302c5773a0b0
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-11-29 23:28:31 -0600

    Add unsecured fix to cors headers

--- index.ts
+++ index.ts
@@ -1,10 +1,24 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
+const cors = require('cors')
 
 // Import API Handlers
 const manageApi = require('./api/ManageApi')
 
+// Enviromental Variables
 dotenv.config()
+const frontendWhitelist = [`${process.env.FRONTEND_URL_CONNECTION}`, 'http://localhost:4000']
+
+// Cors configuration
+const corsOptions = {
+    origin: function (origin: any, callback: any) {
+        if (frontendWhitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
 
 // Configure App
 const app: Express = express()
@@ -12,6 +26,7 @@ const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(express.json())
+app.use(cors()) // Cors middleware
 
 // Routes
 app.get('/', (req: Request, res: Response) => {
