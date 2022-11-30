295dc9bb45c32545cce050b6e792cc3fbfc07f94
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-11-29 23:51:29 -0600

    Possible fix to cors issue

--- api/controller/ManageApiController.ts
+++ api/controller/ManageApiController.ts
@@ -7,8 +7,8 @@ dotenv.config()
 
 // Controllers
 exports.manageApi = (req: Request, res: Response, next: any) => {
-    res.header("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL_CONNECTION}`);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL_CONNECTION}`);
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.status(200).send({msg: "Test message. Backend Server Working Correctly."})
     next()
 }
