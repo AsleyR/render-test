7e9d461d261246711928c390e04ef760ef5108f5
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-11-29 20:44:58 -0600

    Add 404 route error handling

--- index.ts
+++ index.ts
@@ -20,6 +20,11 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use('/api', manageApi)
 
+// 404 Error Handling
+app.all('*', (req: Request, res: Response) => {
+    res.status(400).send("404 Error! Sorry, The Page You Were Looking For Doesn't Exist.")
+})
+
 app.listen(PORT, () => {
     console.log(`[Server]: Server is running at: http://localhost:${PORT}`)
 })
