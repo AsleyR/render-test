25bcbce769d95c60ad9dcf37013aef862f3b61ec
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-11-29 22:38:30 -0600

    Add Acess-Control-Allow-Origin to frontend using .env's

--- api/controller/ManageApiController.ts
+++ api/controller/ManageApiController.ts
@@ -1,5 +1,12 @@
+// Import libraries
 import { Request, Response } from 'express'
+import dotenv from 'dotenv'
 
+// Config .env
+dotenv.config()
+
+// Controllers
 exports.manageApi = (req: Request, res: Response) => {
+    res.header("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL_CONNECTION}`)
     res.status(200).send({msg: "Test message. Backend Server Working Correctly."})
 }
